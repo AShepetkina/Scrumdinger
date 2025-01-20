0068ea306920e0d0b12b860d1b896554cd0b11a0
Anastasia Schepetkina <anastasia.schepetkina@dif.tech>
2025-01-20 20:27:19 +0300

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Anastasia Schepetkina on 20.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
