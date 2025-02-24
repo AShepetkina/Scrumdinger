cec0ed5f943d0db6e8631569a9416f7eab7a3f08
Nikita Kuznetsov <nikita.kuznetsov@dif.tech>
2025-02-24 18:36:56 +0200

    added interesting button

--- Scrumdinger/Views/ScrumsView.swift
+++ Scrumdinger/Views/ScrumsView.swift
@@ -1,31 +1,45 @@
 import SwiftUI
 
 struct ScrumsView: View {
+    @State private var interestingNumber: Int = 0
     @Binding var scrums: [DailyScrum]
     @Environment(\.scenePhase) private var scenePhase
     @State private var isPresentingNewScrumView = false
-    let saveAction: ()->Void
+    let saveAction: () -> Void
 
     var body: some View {
         NavigationStack {
-            List($scrums) { $scrum in
-                NavigationLink(destination: DetailView(scrum: $scrum)) {
-                    CardView(scrum: scrum)
+            VStack {
+                List($scrums) { $scrum in
+                    NavigationLink(destination: DetailView(scrum: $scrum)) {
+                        CardView(scrum: scrum)
+                    }
+                    .listRowBackground(scrum.theme.mainColor)
                 }
-                .listRowBackground(scrum.theme.mainColor)
-            }
-            .navigationTitle("Daily Scrums")
-            .toolbar {
-                Button(action: {
-                    isPresentingNewScrumView = true
-                }) {
-                    Image(systemName: "plus")
+                .navigationTitle("Daily Scrums")
+                .toolbar {
+                    Button(action: {
+                        isPresentingNewScrumView = true
+                    }) {
+                        Image(systemName: "plus")
+                    }
+                    .accessibilityLabel("New Scrum")
                 }
-                .accessibilityLabel("New Scrum")
+
+                Spacer()
+                
+                HStack {
+                    Text("What a number: \(interestingNumber)!")
+                    Button("PUSH ME!!1") {
+                        interestingNumber = Int.random(in: 1...10)
+                    }
+                }
             }
         }
         .sheet(isPresented: $isPresentingNewScrumView) {
-            NewScrumSheet(scrums: $scrums, isPresentingNewScrumView: $isPresentingNewScrumView)
+            NewScrumSheet(
+                scrums: $scrums,
+                isPresentingNewScrumView: $isPresentingNewScrumView)
         }
         .onChange(of: scenePhase) { phase in
             if phase == .inactive { saveAction() }
